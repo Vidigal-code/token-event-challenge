0008a3a0ea9943868a678a069f9566010dfa3d66
Vidigal-code <kauan.vidigal@gmail.com>
2025-06-20 18:31:06 -0300

    tailwindcss init

---
+++ react-token-event-photo/src/App.tsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+      <>
+          <h1 className="text-4xl font-bold text-center text-purple-600 mt-6">tailwindcss init</h1>
+      </>
+  )
+}
+
+export default App
